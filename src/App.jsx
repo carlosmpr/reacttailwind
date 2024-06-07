3e09195a9afdb820fe057c089ffc31cf24daa170
Carlos Polanco <carlospolanco@Carloss-MacBook-Air.local>
2024-06-07 14:01:35 -0400

    react tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className="bg-white flex w-full flex-col justify-center items-center px-16 py-11 max-md:max-w-full max-md:px-5">
+        <div className="flex w-full max-w-[1180px] flex-col mb-3 max-md:max-w-full">
+          <div className="self-stretch flex w-full justify-between gap-5 items-start max-md:max-w-full max-md:flex-wrap">
+            <div className="flex items-start justify-between gap-5 max-md:justify-center">
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/a9d84fec611c8cb7272a0420344dcf014eadd5d7a16f68c10814625115ab0177?apiKey=d97cbe2c7bba48de8f2b84d8472d3ccd&"
+                className="aspect-[2.72] object-contain object-center w-[109px] overflow-hidden self-stretch shrink-0 max-w-full"
+              />
+              <div className="text-gray-600 text-center text-sm self-center my-auto">
+                Features
+              </div>
+              <div className="text-gray-600 text-center text-sm self-center my-auto">
+                Testimonials
+              </div>
+              <div className="text-gray-600 text-center text-sm self-center whitespace-nowrap my-auto">
+                Pricing
+              </div>
+            </div>
+            <div className="self-stretch flex items-center justify-between gap-5">
+              <div className="text-gray-600 text-center text-sm grow whitespace-nowrap my-auto">
+                Sign in
+              </div>
+              <div className="text-white text-center text-sm font-medium whitespace-nowrap justify-center items-stretch bg-blue-600 self-stretch grow px-3.5 py-3 rounded-[50px]">
+                Get started today
+              </div>
+            </div>
+          </div>
+          <div className="text-blue-600 text-center text-7xl font-medium leading-[81px] tracking-tighter max-w-[810px] self-center mt-44 max-md:max-w-full max-md:text-4xl max-md:leading-[52px] max-md:mt-10">
+            <span className="text-gray-900">Accounting </span>
+            <span className="text-blue-600">
+              made simple
+              <br />
+            </span>
+            <span className="text-gray-900">for small businesses.</span>
+          </div>
+          <div className="text-gray-600 text-center text-lg leading-8 tracking-normal self-center max-w-[602px] mt-11 max-md:max-w-full max-md:mt-10">
+            Most bookkeeping software is accurate, but hard to use. We make the
+            opposite trade-off, and hope you don’t get audited.
+          </div>
+          <div className="self-center flex items-stretch gap-5 mt-11 px-px max-md:mt-10">
+            <div className="text-white text-center text-sm font-medium whitespace-nowrap justify-center items-stretch bg-gray-900 grow px-4 py-3 rounded-[50px]">
+              Get 6 months free
+            </div>
+            <div className="justify-between items-center border border-[color:var(--cool-gray-200,#E5E7EB)] bg-white flex gap-2.5 px-5 py-3 rounded-[50px] border-solid">
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/e38e62b96434947914ab21e2bafe689a0cd327b3df8ea614e872cbbd57988766?apiKey=d97cbe2c7bba48de8f2b84d8472d3ccd&"
+                className="aspect-[0.91] object-contain object-center w-2.5 fill-blue-600 overflow-hidden shrink-0 max-w-full my-auto"
+              />
+              <div className="text-gray-600 text-center text-sm font-medium self-stretch grow whitespace-nowrap">
+                Watch video
+              </div>
+            </div>
+          </div>
+          <div className="text-gray-900 text-center text-base font-medium tracking-tight self-center whitespace-nowrap mt-44 max-md:mt-10">
+            Trusted by these six companies so far
+          </div>
+          <div className="items-stretch self-center flex justify-between gap-5 mt-9 max-md:max-w-full max-md:flex-wrap max-md:justify-center">
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/d6afdd66a440fbc9a46b41e0c18f3907c8038dc4d19d1ca92a729fb2342bbbbd?apiKey=d97cbe2c7bba48de8f2b84d8472d3ccd&"
+              className="aspect-[3.29] object-contain object-center w-[158px] overflow-hidden shrink-0 max-w-full"
+            />
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/99376388f3f85113d4fdad12c4f03a8100dc2edee442e1301dae3004d8ce642e?apiKey=d97cbe2c7bba48de8f2b84d8472d3ccd&"
+              className="aspect-[2.19] object-contain object-center w-[105px] overflow-hidden shrink-0 max-w-full"
+            />
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/96a68282886bc25357d28f98f79c9805fea92f9d959c1514a94dfdb3d86ce523?apiKey=d97cbe2c7bba48de8f2b84d8472d3ccd&"
+              className="aspect-[2.65] object-contain object-center w-[127px] overflow-hidden shrink-0 max-w-full"
+            />
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/ff4d86ffb32d3a5d30de6c78f2d07bbdf22085427c4e23d9b75da58a9c47766f?apiKey=d97cbe2c7bba48de8f2b84d8472d3ccd&"
+              className="aspect-[2.88] object-contain object-center w-[138px] overflow-hidden shrink-0 max-w-full"
+            />
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/63afbec1913ad5259cd3a74081a8749bd78c05f800acdfbc46638206e3fae436?apiKey=d97cbe2c7bba48de8f2b84d8472d3ccd&"
+              className="aspect-[2.83] object-contain object-center w-[136px] overflow-hidden shrink-0 max-w-full"
+            />
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/18d1c39a1f427ed4e07e13649e11309350ff2306a536bd3f769740eff50cbbd2?apiKey=d97cbe2c7bba48de8f2b84d8472d3ccd&"
+              className="aspect-[3.06] object-contain object-center w-[147px] overflow-hidden shrink-0 max-w-full"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
